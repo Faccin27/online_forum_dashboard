da2054267f25209c80b08151ea6f7f6529e2f09a
GUILHERME FACCIN <guilherme_faccin@estudante.sesisenai.org.br>
2024-06-11 19:57:00 -0300

    Controle de login conectado ao bdd com validação de dados implementado.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const router = new Router();
 const RegisterController = require('../controllers/RegisterController');
+const LoginController = require('../controllers/LoginController');
 
 router.get('/', (req, res) => {
-  res.status(200).redirect("login")
-})
+  res.status(200).redirect("login");
+});
 
 router.get('/login', (req, res) => {
   res.status(200).render("login");
@@ -19,6 +20,6 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', RegisterController.register);
-
+router.post('/login', LoginController.login); // Add this line
 
 module.exports = router;
